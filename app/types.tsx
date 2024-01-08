839ffe5c9fbb61fb47c687368709f0c0b1d9fe6e
Igor Paes <igor.paes@nextip.com.br>
2024-01-08 00:50:35 -0300

    feat: tiktok clone design

---
+++ app/types.tsx
@@ -0,0 +1,130 @@
+export interface SingleCommentCompTypes {
+  params: { userId: string; postId: string };
+  comment: CommentWithProfile;
+}
+
+export interface CommentWithProfile {
+  id: string;
+  user_id: string;
+  post_id: string;
+  text: string;
+  created_at: string;
+  profile: {
+    user_id: string;
+    name: string;
+    image: string;
+  };
+}
+
+export interface CommentsCompTypes {
+  params: { userId: string; postId: string };
+}
+
+export interface CommentsHeaderCompTypes {
+  params: { userId: string; postId: string };
+  post: PostWithProfile;
+}
+
+export interface PostPageTypes {
+  params: { userId: string; postId: string };
+}
+
+export interface TextInputCompTypes {
+  string: string;
+  inputType: string;
+  placeholder: string;
+  onUpdate: (newValue: string) => void;
+  error: string;
+}
+
+export interface CropperDimensions {
+  height?: number | null;
+  width?: number | null;
+  left?: number | null;
+  top?: number | null;
+}
+
+export interface ShowErrorObject {
+  type: string;
+  message: string;
+}
+
+export interface PostUserCompTypes {
+  post: {
+    id: string;
+    user_id: string;
+    video_url: string;
+    text: string;
+    created_at: string;
+  };
+}
+
+export interface ProfilePageTypes {
+  params: { id: string };
+}
+
+export interface UploadError {
+  type: string;
+  message: string;
+}
+
+export interface Post {
+  id: string;
+  user_id: string;
+  video_id: string;
+  text: string;
+  created_at: string;
+}
+
+export interface Like {
+  id: string;
+  user_id: string;
+  post_id: string;
+}
+
+export interface Comment {
+  id: string;
+  user_id: string;
+  post_id: string;
+  text: string;
+  created_at: string;
+}
+
+export interface RandomUsers {
+  id: string;
+  name: string;
+  image: string;
+}
+
+export interface PostMainCompTypes {
+  post: PostWithProfile;
+}
+
+export interface PostWithProfile {
+  id: string;
+  user_id: string;
+  video_url: string;
+  text: string;
+  created_at: string;
+  profile: {
+    user_id: string;
+    name: string;
+    image: string;
+  };
+}
+
+export interface PostMainLikesCompTypes {
+  post: PostWithProfile;
+}
+
+// LAYOUT INCLUDE TYPES
+
+export interface MenuItemsTypes {
+  iconString: string;
+  colorString: string;
+  sizeString: string;
+}
+
+export interface MenuItemFollowCompTypes {
+  user: RandomUsers;
+}
